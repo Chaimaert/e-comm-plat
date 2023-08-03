88f58a46d9d1943e647cfdb53b8ac245f3039e4b
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-08-03 20:34:15 +0100

    setting up the header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,49 +1,70 @@
 import React from "react"
 import { BiSolidPhoneCall } from 'react-icons/bi'
-import { AiOutlineMail } from 'react-icons/ai'
+import { AiOutlineInstagram, AiOutlineTwitter } from 'react-icons/ai'
+import { RiFacebookCircleFill } from 'react-icons/ri'
+import { BiLogoGmail } from 'react-icons/bi'
 import { Link } from "react-router-dom"
+import { FaClipboardList } from 'react-icons/fa'
+import { MdAccountCircle } from 'react-icons/md'
+import { PiShoppingCartSimpleFill } from 'react-icons/pi'
 import logo from '../assets/images/apneck.png'
 
 
 const header = () => {
   return <>
-    <header className="header-top-strip p-1 shadow-sm">
+    <header className="header-top-strip px-4 p-1 shadow-sm">
       <div className="container-xxl">
         <div className="row align-items-center">
           <div className="col-6">
             <p>The trending outfits at 100% off</p>
           </div>
           <div className="col-6 d-flex justify-content-between">
-            <div>
-            <a href="tel:+212661085174" color="black"> <BiSolidPhoneCall /> Call us +212661085174 </a>
-            </div>
-            <div>
-              <Link><BiSolidPhoneCall className="fs-3 mx-4" size={20}  /></Link>
-              <Link><AiOutlineMail className="fs-3 mx-4" size={20} /></Link>
-            </div>
+              <div>
+                <a href="tel:+212661085174" color="black"> <BiSolidPhoneCall /> Call us +212661085174 </a>
+              </div>
+              <div>
+                <Link><AiOutlineInstagram className="fs-3 mx-2" size={20} /></Link>
+                <Link><RiFacebookCircleFill className="fs-3 mx-2" size={20}  /></Link>
+                <Link><AiOutlineTwitter className="fs-3 mx-2" size={20}  /></Link>
+                <Link><BiLogoGmail className="fs-3 mx-2" size={20} /></Link>
+              </div>
           </div>
         </div>
       </div>
 
     </header>
   
-    <header className="header-upper">
+    <header className="header-upper px-3">
       <div className="container-xxl">
         <div className="row">
-          <div className="col-2">
+          <div className="col-2 text-center m-auto">
           <Link to='/'><img src={logo} className="img-fluid logo" /></Link>
           </div>
-          <div className="col-3">
-            
+          <div className="col-3 d-flex align-items-center mt-3">
+              <div className="input-group mb-3">
+                  <span className="input-group-text" id="basic-addon1">All</span>
+                  <input type="text" className="form-control p-2" placeholder="Search Products" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                  <button className="input-group-text" id="basic-addon2">Search</button>
+              </div>
           </div>
-          <div className="col-4 d-flex align-items-center justify-content-between">
+          <div className="nav-links col-4 d-flex align-items-center justify-content-between text-center m-auto">
             <Link to={'/'}>Home</Link>
             <Link to={'shop'}>Shop</Link>
             <Link to={'blog'}>Blog</Link>
             <Link to={'about'}>About</Link>
             <Link to={'contact'}>Contact</Link>
           </div>
-          <div className="col-5"></div>
+          <div className="col-3 d-flex align-items-center justify-content-around">
+                <Link className="d-flex"><FaClipboardList className="mx-2 m-auto"/>
+                <p>WishList</p>
+                </Link>
+                <Link className="d-flex"><MdAccountCircle className=" mx-2 m-auto"/>
+                <p>Account</p>
+                </Link>
+                <Link className="d-flex"><PiShoppingCartSimpleFill className=" mx-2 m-auto"/>
+                <p>Cart</p>
+                </Link>
+          </div>
         </div>
       </div>
     </header>
