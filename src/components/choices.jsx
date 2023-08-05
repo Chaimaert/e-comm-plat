a92ec98c0bd04dfe4419ac0d3a2b96ae662ef13e
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-08-05 13:50:26 +0100

    Home Update

--- src/components/choices.jsx
+++ src/components/choices.jsx
@@ -1,9 +1,8 @@
-import icon1 from '../assets/images/icons/icon1.png'
-import icon2 from '../assets/images/icons/icon2.png'
-import icon3 from '../assets/images/icons/icon3.png'
-import icon4 from '../assets/images/icons/icon4.png'
-import icon5 from '../assets/images/icons/icon5.png'
-import icon6 from '../assets/images/icons/icon6.png'
+import courier from '../assets/images/icons/courier.png'
+import truck from '../assets/images/icons/truck.png'
+import saving from '../assets/images/icons/save-money.png'
+import support from '../assets/images/icons/support.png'
+import discount from '../assets/images/icons/gift-box.png'
 
 const choices = () => {
   return <>
@@ -14,22 +13,24 @@ const choices = () => {
         <div className="hero-details">
           <div className="hero-details d-flex justify-content-around text-center">
             <div className='card p-2'>
-              <img src={icon1} alt="" className='img-fluid' />
+              <img src={courier} alt="" className='img-fluid' />
+              <p>Fast Orders</p>
             </div>
             <div className='card p-2'>
-              <img src={icon2} alt="" className='img-fluid' />
+              <img src={truck} alt="" className='img-fluid' />
+              <p>Quick Shipping</p>
             </div>
             <div className='card p-2'>
-              <img src={icon3} alt="" className='img-fluid' />
+              <img src={saving} alt="" className='img-fluid' />
+              <p>High Saves</p>
             </div>
             <div className='card p-2'>
-              <img src={icon4} alt="" className='img-fluid' />
+              <img src={support} alt="" className='img-fluid' />
+              <p>24/7 Support</p>
             </div>
             <div className='card p-2'>
-              <img src={icon5} alt="" className='img-fluid' />
-            </div>
-            <div className='card p-2'>
-              <img src={icon6} alt="" className='img-fluid' />
+              <img src={discount} alt="" className='img-fluid' />
+              <p>Discounts</p>
             </div>
           </div>
         </div>
