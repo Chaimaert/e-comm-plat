85e3e7206e73ee94391e14a471e61041423a0882
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-08-06 15:40:04 +0100

    update

--- src/components/featuredproducts.jsx
+++ src/components/featuredproducts.jsx
@@ -3,14 +3,25 @@ import { PRODUCTS } from './products'
 const featuredproducts = () => {
   return <>
   
-  <div className="card">
+  <div className="row">
       {PRODUCTS.slice(2,6).map(product => (
-        <img src={product.image} alt="" />
+        <div className="col-3">
+            <div key={product.id}>
+            <div className="card text-center">
+            <img src={product.image} alt="" className='card-img-top img-fluid m-auto' />
+            <div className="card-details">
+                <span> {product.brand} </span>
+                <h4> {product.name} </h4>
+                <div className="card-bottom">
+                    <p> {product.status} </p>
+                    <p> {product.rate} </p>
+                </div>
+            </div>
+            </div>
+            </div>
+          </div>
       ))}
   </div>
-  
-
   </>;
 }
-
 export default featuredproducts;
