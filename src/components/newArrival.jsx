34f850a847a4211bd91acba04744852522f47d0d
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-08-09 14:53:20 +0100

    update

---
+++ src/components/newArrival.jsx
@@ -0,0 +1,28 @@
+import { PRODUCTS } from './products';
+
+const newArrival = () => {
+  return <>
+      <div className="row mt-4">
+      {PRODUCTS.slice(2, 6).map((product) => (
+        <div className="col-3" key={product.id}>
+          <div className="card h-100 m-auto p-3">
+            <img src={product.image} alt="" className="card-img-top img-fluid m-auto p-3" />
+            <div className="card-details">
+                <div className="title mb-3">
+                  <span> {product.brand} </span>
+                  <h5 className='mt-2'> {product.name} </h5>
+                </div>
+              <div className="card-footer text-center mb-2">
+                <p className='mb-2'> {product.status} </p>
+                <p className='text-danger'> <strike> {product.rate} </strike> </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  
+  </>
+}
+
+export default newArrival;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -25,7 +25,7 @@ const home = () => {
           <div className="row">
             <div className="text-center">
               <h1>Our Featured Products</h1>
-              <p>Modern Designs</p>
+              <p className="md-design">Modern and Latests Designs</p>
             </div>
             <Featuredproducts  />
           </div>
@@ -37,13 +37,25 @@ const home = () => {
         <div className="row">
           <div className="repair-details text-center d-flex flex-column align-items-center p-3">
               <span>Repair services</span>
-              <h2>Find out our big <span className="text-danger">Discounts</span></h2>
+              <h2 className="mt-3 disc"> Find out our big <span className="text-danger disc">Discounts</span></h2>
               <div className="mt-3">
                 <button className="btn-repair">Explore Now</button>
               </div>
           </div>
         </div>
       </div>
+    </section>
+
+    <section className="new-products p-5">
+        <div className="cintainer-xxl">
+          <div className="row">
+            <div className="text-center">
+              <h1 className="disc">New Arrivals</h1>
+              <p className="md-design">Modern and Latests Designs</p>
+            </div>
+            <Featuredproducts  />
+          </div>
+        </div>
     </section>
   </>
 };
