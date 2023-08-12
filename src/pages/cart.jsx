8f54e1ba69a99470a73cf8ba0d4752cbe9782a5a
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-08-12 18:49:23 +0100

    update

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,13 +1,15 @@
 import { PRODUCTS, PRODUCTS1 } from '../components/products'
-import Cartitems from '../components/Cartitems'
+import Cartitems from '../components/cartitems'
+import { useContext } from "react"; 
 import { ShopContext } from '../components/shopcontext';
 
 
-const cart = () => {
+const cart = (props) => {
+  const { viewProductDetails, addToCart, cartItems } = useContext(ShopContext);
   return <>
     <div className="row">
         {[...PRODUCTS, ...PRODUCTS1].map((product) => {
-            if (CartItems[product.id] !== 0) {
+            if (cartItems[product.id] !== 0) {
               return <Cartitems key={product.id} data={product} />
             }
         })}
