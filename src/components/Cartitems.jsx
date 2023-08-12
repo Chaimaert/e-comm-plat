baddbabac95ba8b7ebd73b2efbeedc057bd754ea
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-08-12 18:34:28 +0100

    update

--- src/components/Cartitems.jsx
+++ src/components/Cartitems.jsx
@@ -1,9 +1,41 @@
+import { useContext } from "react"; 
+import { ShopContext } from './shopcontext'
 
 
+const Cartitems = (props) => {
 
-const Cartitems = () => {
+  const {id, name, image, price, brand} = props.data;
+  const { viewProductDetails, addToCart, cartItems } = useContext(ShopContext);
+
+  <section className="cart-item p-5">
+    <div className="container-xxl">
+      <div key={id} className="row">
+        <table>
+          <thead>
+            <th>Product Image</th>
+            <th>Product Details</th>
+            <th>Edit</th>
+            <th>Coupons</th>
+          </thead>
+          <tbody>
+            <tr>
+                <td>
+                  <img src={image} alt="" className='img-fluid' />
+                </td>
+                <td>
+                  <p> {brand} </p>
+                  <p> {name} </p>
+                  <p> {price} </p>
+                </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+  </section>
   return <>
-  
+    
   </>
 }
 
--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,9 +1,18 @@
+import { PRODUCTS, PRODUCTS1 } from '../components/products'
+import Cartitems from '../components/Cartitems'
+import { ShopContext } from '../components/shopcontext';
 
 
 const cart = () => {
-  return (
-    <div> Cart </div>
-  );
+  return <>
+    <div className="row">
+        {[...PRODUCTS, ...PRODUCTS1].map((product) => {
+            if (CartItems[product.id] !== 0) {
+              return <Cartitems key={product.id} data={product} />
+            }
+        })}
+    </div>
+  </>
 };
 
 export default cart;
