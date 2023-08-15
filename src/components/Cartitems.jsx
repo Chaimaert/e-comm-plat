fef1977b7964dbc6a9a888c21d22721e753cbc71
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-08-15 12:16:28 +0100

    updating the cart

--- src/components/Cartitems.jsx
+++ src/components/Cartitems.jsx
@@ -1,42 +1,61 @@
-import { useContext } from "react"; 
+import { useContext } from "react"
 import { ShopContext } from './shopcontext'
 
 
-const Cartitems = (props) => {
+const cartitems = (props) => {
 
   const {id, name, image, price, brand} = props.data;
-  const { viewProductDetails, addToCart, cartItems } = useContext(ShopContext);
+  const { viewProductDetails, addToCart, cartItems, removeToCart, updateCartItemCount } = useContext(ShopContext);
 
-  <section className="cart-item p-5">
-    <div className="container-xxl">
-      <div key={id} className="row">
-        <table>
-          <thead>
-            <th>Product Image</th>
-            <th>Product Details</th>
-            <th>Edit</th>
-            <th>Coupons</th>
-          </thead>
-          <tbody>
-            <tr>
-                <td>
-                  <img src={image} alt="" className='img-fluid' />
-                </td>
-                <td>
-                  <p> {brand} </p>
-                  <p> {name} </p>
-                  <p> {price} </p>
-                </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-
-  </section>
   return <>
+    <tbody key={id}>
+       <tr>
+                        <td>
+                            <div className="p-2">
+                                <img src={image} alt="" className='img-fluid' />
+                            </div>
+                        </td>
+                        <td>
+                            <p> Brand :  <span className="price"> {brand}</span> </p>
+                            <p> Name : <h5 className="price">{name}</h5></p>
+                            <p> Price : <span className="price"> {price}</span></p>
+                            <p>Items in Stock : <span className="text-danger">1200</span></p>
+                        </td>
+                        <td>
+                            <button
+                                className="add-btn mx-2"
+                                onClick={() => {
+                                  addToCart(id);
+
+                                  }
+                                }>+
+                             </button>
+
+                             <input type="text" value={cartItems[id]} onChange={(e) => 
+                            updateCartItemCount(Number(e.target.value), id)} />
+
+                             <button
+                                className="add-btn mx-2"
+                                onClick={() => {
+                                  removeToCart(id);
+
+                                  }
+                                }>-
+                             </button>
+                        </td>
+
+                        <td>
+                          <div className="input-group mb-3">
+                          <input type="text" className="form-control" placeholder="Enter coupon code ..." 
+                              aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                              <button className="input-group-text" id="basic-addon2">Search</button>
+                          </div>
+                        </td>
+                    </tr>
+                    <hr />
+    </tbody>
     
-  </>
+   </>
 }
 
-export default Cartitems
+export default cartitems
--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -7,13 +7,27 @@ import { ShopContext } from '../components/shopcontext';
 const cart = (props) => {
   const { viewProductDetails, addToCart, cartItems } = useContext(ShopContext);
   return <>
-    <div className="row">
-        {[...PRODUCTS, ...PRODUCTS1].map((product) => {
-            if (cartItems[product.id] !== 0) {
-              return <Cartitems key={product.id} data={product} />
-            }
-        })}
-    </div>
+    <section className="cart-item p-5">
+            <div className="container-xxl">
+              <div className="row">
+                    <table>
+                      <thead className="my-2">
+                                    <th className="col-3">Product Image</th>
+                                    <th className="col-3">Product Details</th>
+                                    <th className="col-3">Edit</th>
+                                    <th className="col-3">Coupons</th>
+                        </thead>
+                            {[...PRODUCTS, ...PRODUCTS1].map((product) => {
+                              if (cartItems[product.id] !== 0) {
+                                return <Cartitems key={product.id} data={product} />
+                              }
+                            })}
+                    </table>
+                </div>
+             </div>
+
+
+    </section>
   </>
 };
 
