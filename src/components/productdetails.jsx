7de889fe78123de4864128169e800a2a7c80904a
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-08-21 17:22:51 +0100

    update

--- src/components/productdetails.jsx
+++ src/components/productdetails.jsx
@@ -6,7 +6,7 @@ import { PRODUCTS, PRODUCTS1 } from './products'
 
 const productdetails = () => {
   
-  const { viewProductDetails, addToCart, cartItems, closeProductDetails, selectedProduct } = useContext(ShopContext);
+  const { updateCartItemCount, removeToCart, addToCart, cartItems, closeProductDetails, selectedProduct } = useContext(ShopContext);
   const productId = selectedProduct || 0;
   const product = PRODUCTS.find((p) => p.id === productId) || PRODUCTS1.find ((p) => p.id === productId);
   
