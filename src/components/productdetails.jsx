eea47dd121c046d6aed7f34117fad8606a871d5e
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-08-21 17:41:09 +0100

    Update

--- src/components/productdetails.jsx
+++ src/components/productdetails.jsx
@@ -19,17 +19,18 @@ const productdetails = () => {
 
     <div className="col-6">
         <div className="p-4">
-              <span> {product.brand} </span>
-              <h5> {product.name} </h5>
+              <span className="bd-name"> {product.brand} </span>
+              <h5 className="my-3"> {product.name} </h5>
               <div className="card-details">
-                  <p>Was <strike className="text-danger"> {product.price*2} </strike> &nbsp; Now
-                      <span className="price"> ${product.price} </span>
+                  <p className="my-3">Was <strike className="text-danger"> {product.price*2} </strike> &nbsp;Now
+                      <span className="price">    ${ product.price} </span>
                   </p>
 
-                  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum eius delectus officia eaque accusamus? Tempore,
+                  <p className="my-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum eius delectus officia eaque accusamus? Tempore,
                     tempora nostrum accusamus vel aliquam at officiis? 
                     Omnis sit dolorum consequuntur commodi dicta facilis delectus?</p>
               </div>
+                  
                     <button
                                 className="add-btn mx-2"
                                 onClick={() => {
