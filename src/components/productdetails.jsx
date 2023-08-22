79f193e6ed438055dcba5b5f501908919e17f2f1
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-08-22 13:05:22 +0100

    update

--- src/components/productdetails.jsx
+++ src/components/productdetails.jsx
@@ -59,7 +59,26 @@ const productdetails = () => {
                       </div>
                     
                     <div className="card-footer">
-                      <h3 className="price my-3">More products</h3>
+                        <h3 className="price my-3 text-center more-prod">More products</h3>
+                        <div className="row mt-4">
+                            {PRODUCTS.slice(2, 6).map((product) => (
+                              <div className="col-3" key={product.id}>
+                                <div className="card h-100 m-auto p-3">
+                                  <img src={product.image} alt="" className="card-img-top img-fluid m-auto p-3" />
+                                  <div className="card-details">
+                                      <div className="title mb-3">
+                                        <span> {product.brand} </span>
+                                        <h5 className='mt-2'> {product.name} </h5>
+                                      </div>
+                                    <div className="card-footer text-center mb-2">
+                                      <p className='mb-2'> {product.status} </p>
+                                      <p className='text-danger'> <strike> {product.rate} </strike> </p>
+                                    </div>
+                                </div>
+                              </div>
+                              </div>
+                            ))}
+                        </div>
                     </div>
         </div>
     </div>
