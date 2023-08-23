d6d594c52668feaaa0a2f31d7264c46b2f93bb5f
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-08-23 13:22:42 +0100

    starting the contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,8 +1,17 @@
-
-
 const contact = () => {
   return (
-    <div> Home </div>
+    <>
+      <section className="contact-wrapper p-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12 text-center">
+              <h1 className=" text">Reach Out to Us</h1>
+              <p className=" text fs-3">We are only a step away from you</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
