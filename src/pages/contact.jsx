06eae92a5f719d4c48deb89a7e0972bf159f7b11
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-08-23 13:27:19 +0100

    Update

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -6,7 +6,7 @@ const contact = () => {
           <div className="row">
             <div className="col-12 text-center">
               <h1 className=" text">Reach Out to Us</h1>
-              <p className=" text fs-3">We are only a step away from you</p>
+              <p className="text2 fs-3">We are only a step away from you</p>
             </div>
           </div>
         </div>
