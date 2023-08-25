93ca8c522b38ad3314707848b29ea0320276d9e0
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-08-25 21:33:38 +0100

    Update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,19 @@
 import { useContext, useState } from "react";
 import { ShopContext } from "./shopcontext";
-// import { BiSolidPhoneCall } from "react-icons/bi";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 import { RiFacebookCircleFill } from "react-icons/ri";
 import { BiLogoGmail } from "react-icons/bi";
-import { Link, useLocation } from "react-router-dom";
+import { NavLink, Link, useLocation } from "react-router-dom";
 import { FaClipboardList } from "react-icons/fa";
 import { MdAccountCircle } from "react-icons/md";
 import { PiShoppingCartSimpleFill } from "react-icons/pi";
+import compare from "../assets/images/compare.svg";
+import wishlist from "../assets/images/wishlist.svg";
+import user from "../assets/images/user.svg";
 import Mylogo from "../assets/images/Mylogo.png";
 
-const Header = () => {
+const header = () => {
   const [showMenu, setShowMenu] = useState(false);
   const { getTotalCartProducts, getTotalCartAmount } = useContext(ShopContext);
   const totalAmount = getTotalCartAmount();
@@ -49,104 +52,295 @@ const Header = () => {
         </div>
       </header>
 
-      <header className='navbar-middle sticky-top p-2 p-md-2 p-lg-2'>
+      <header className="navbar-middle sticky-top p-2 p-md-2 p-lg-2">
         <div className="container-xxl">
-          <div className="row">
-            <div className="col-2 text-center m-auto">
+          <div className="row align-items-center m-0">
+            <div className="col-md-2 d-flex justify-content-center">
+              <button
+                className="navbar-toggler d-md-none "
+                type="button"
+                onClick={toggleMenu}
+              >
+                <span className="navbar-toggler-icon">
+                  {showMenu ? <AiOutlineClose /> : <AiOutlineMenu />}
+                </span>
+              </button>
+
               <Link to="/">
                 <img src={Mylogo} className="img-fluid logo" />
               </Link>
+
+              <button className="cart-span fs-3 d-md-none">
+                <Link
+                  to="cart"
+                  className={
+                    location.pathname === "/cart" ? "active" : "not-active"
+                  }
+                >
+                  <PiShoppingCartSimpleFill />
+                  <b>
+                    <span>{totalProducts}</span>
+                  </b>
+                </Link>
+              </button>
+
+              <button className="cart-span-2 fs-3 d-md-none">
+                <Link
+                  to="login"
+                  className={
+                    location.pathname === "/login" ? "active" : "not-active"
+                  }
+                >
+                  <MdAccountCircle />
+                </Link>
+              </button>
             </div>
-            <div className="col-3 d-flex align-items-center mt-3">
-              <div className="input-group mb-3">
-                <span className="input-group-text" id="basic-addon1">
-                  All
-                </span>
-                <input
-                  type="text"
-                  className="form-control p-2"
-                  placeholder="Search Products"
-                  aria-label="Recipient's username"
-                  aria-describedby="basic-addon2"
-                />
-                <button className="input-group-text" id="basic-addon2">
-                  Search
-                </button>
+
+            <div className="col-md-10 row col-lg-10">
+              <div className="col-md-3 m-auto">
+                <div className="input-group d-none d-md-flex">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Find products ..."
+                    aria-label="Find products ..."
+                    aria-describedby="basic-addon2"
+                  />
+                  <button className="input-group-text" id="basic-addon2">
+                    search
+                  </button>
+                </div>
+              </div>
+
+              <div className="col-md-6 m-auto">
+                <div className="menu-links mt-2 d-none d-md-flex d-lg-flex">
+                  <div className="ms-auto gap-3">
+                    <NavLink
+                      to="/"
+                      className={
+                        location.pathname === "/" ? "active" : "not-active"
+                      }
+                      onClick={toggleMenu}
+                    >
+                      HOME
+                    </NavLink>
+                  </div>
+                  <div className="ms-auto gap-3">
+                    <NavLink
+                      to="/shop"
+                      className={
+                        location.pathname === "/shop" ? "active" : "not-active"
+                      }
+                      onClick={toggleMenu}
+                    >
+                      SHOP
+                    </NavLink>
+                  </div>
+                  <div className="ms-auto gap-3">
+                    <NavLink
+                      to="/blog"
+                      className={
+                        location.pathname === "/blog" ? "active" : "not-active"
+                      }
+                      onClick={toggleMenu}
+                    >
+                      BLOG
+                    </NavLink>
+                  </div>
+                  <div className="ms-auto gap-3">
+                    <NavLink
+                      to="/about"
+                      className={
+                        location.pathname === "/about" ? "active" : "not-active"
+                      }
+                      onClick={toggleMenu}
+                    >
+                      ABOUT
+                    </NavLink>
+                  </div>
+                  <div className="ms-auto gap-3">
+                    <NavLink
+                      to="/contact"
+                      className={
+                        location.pathname === "/contact"
+                          ? "active"
+                          : "not-active"
+                      }
+                      onClick={toggleMenu}
+                    >
+                      CONTACT
+                    </NavLink>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-3">
+                <div className="row d-flex justify-content-center">
+                  <div className="col-12 col-md-2 d-none d-md-flex d-lg-flex m-auto">
+                    <div
+                      className={
+                        location.pathname === "/" ? "active" : "not-active"
+                      }
+                    >
+                      <Link
+                        onClick={toggleMenu}
+                        to=""
+                        className="d-flex align-items-center color-nav me-3"
+                      >
+                        <FaClipboardList className="me-1 fs-2" />
+                      </Link>
+                    </div>
+                    <div
+                      className={
+                        location.pathname === "login" ? "active" : "not-active"
+                      }
+                    >
+                      <Link
+                        onClick={toggleMenu}
+                        to="/login"
+                        className="d-flex align-items-center color-nav me-3"
+                      >
+                        <MdAccountCircle className="me-1 fs-2" />
+                      </Link>
+                    </div>
+                    <div
+                      className={
+                        location.pathname === "cart" ? "active" : "not-active"
+                      }
+                    >
+                      <Link
+                        onClick={toggleMenu}
+                        to="/cart"
+                        className="d-flex align-items-center color-nav me-3 cart-span-one"
+                      >
+                        <PiShoppingCartSimpleFill className="me-1 fs-2" />
+                        <div>
+                          <p>
+                            <b>
+                              <span>{totalProducts}</span>
+                            </b>
+                          </p>
+                        </div>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="nav-links col-4 d-flex align-items-center justify-content-between text-center m-auto">
-              <Link
-                to={"/"}
-                className={location.pathname === "/" ? "active" : "inactive"}
-              >
-                Home
-              </Link>
-              <Link
-                to={"shop"}
-                className={
-                  location.pathname === "/shop" ? "active" : "inactive"
-                }
-              >
-                Shop
-              </Link>
-              {/* <Link to={'blog'} className={location.pathname === '/blog' ? 'active' : 'inactive'}>Blog</Link> */}
-              <Link
-                to={"about"}
-                className={
-                  location.pathname === "/about" ? "active" : "inactive"
-                }
-              >
-                About
-              </Link>
-              <Link
-                to={"contact"}
-                className={
-                  location.pathname === "/contact" ? "active" : "inactive"
-                }
-              >
-                Contact
-              </Link>
-            </div>
-            <div className="col-3 d-flex align-items-center justify-content-around">
-              <Link
-                to={"wishlist"}
-                className={
-                  location.pathname === "/wishlist" ? "active" : "inactive"
-                }
-              >
-                <div className="d-flex">
-                  <FaClipboardList className="mx-2 m-auto" />
-                  <p>WishList</p>
+            {showMenu && (
+              <div className="col-md-10 d-md-none mt-3">
+                <div className="input-group mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Find products ..."
+                    aria-label="Find products ..."
+                    aria-describedby="basic-addon2"
+                  />
+                  <button className="input-group-text" id="basic-addon2">
+                    search
+                  </button>
                 </div>
-              </Link>
-              <Link
-                to={"login"}
-                className={
-                  location.pathname === "/login" ? "active" : "inactive"
-                }
-              >
-                <div className="d-flex">
-                  <MdAccountCircle className=" mx-2 m-auto" />
-                  <p>Account</p>
+                <div className="menu-links mt-2">
+                  <div className="mb-2">
+                    <NavLink
+                      className={
+                        location.pathname === "/" ? "active" : "not-active"
+                      }
+                      to="/"
+                      onClick={toggleMenu}
+                    >
+                      HOME
+                    </NavLink>
+                  </div>
+                  <div className="mb-2">
+                    <NavLink
+                      className={
+                        location.pathname === "/shop" ? "active" : "not-active"
+                      }
+                      to="/shop"
+                      onClick={toggleMenu}
+                    >
+                      SHOP
+                    </NavLink>
+                  </div>
+                  <div className="mb-2">
+                    <NavLink
+                      className={
+                        location.pathname === "/blog" ? "active" : "not-active"
+                      }
+                      to="/blog"
+                      onClick={toggleMenu}
+                    >
+                      BLOG
+                    </NavLink>
+                  </div>
+                  <div className="mb-2">
+                    <NavLink
+                      className={
+                        location.pathname === "/about" ? "active" : "not-active"
+                      }
+                      to="/about"
+                      onClick={toggleMenu}
+                    >
+                      ABOUT
+                    </NavLink>
+                  </div>
+                  <div className="mb-2">
+                    <NavLink
+                      className={
+                        location.pathname === "/contact"
+                          ? "active"
+                          : "not-active"
+                      }
+                      to="/contact"
+                      onClick={toggleMenu}
+                    >
+                      CONTACT
+                    </NavLink>
+                  </div>
                 </div>
-              </Link>
-              <Link
-                to={"cart"}
-                className={
-                  location.pathname === "/cart" ? "active" : "inactive"
-                }
-              >
-                <div className="d-flex">
-                  <PiShoppingCartSimpleFill className=" mx-2 m-auto" />
-                  <p>
-                    Cart{" "}
-                    <span className="text-danger">
-                      {totalProducts > 0 && `(${totalProducts})`}
-                    </span>
-                  </p>
+
+                <div className="menu-bar__actions">
+                  <div className="mb-2">
+                    <Link
+                      to=""
+                      className={
+                        location.pathname === "/" ? "active" : "not-active"
+                      }
+                      onClick={toggleMenu}
+                    >
+                      <img src={compare} alt="compare" className="d-none" />
+                      <span>New</span>
+                    </Link>
+                  </div>
+                  <div className="mb-2">
+                    <Link
+                      to=""
+                      className={
+                        location.pathname === "/" ? "active" : "not-active"
+                      }
+                      onClick={toggleMenu}
+                    >
+                      <img src={wishlist} alt="wishlist" className="d-none" />
+                      <span>Wishlist</span>
+                    </Link>
+                  </div>
+                  <div className="mb-2">
+                    <Link
+                      to="/login"
+                      className={
+                        location.pathname === "/login" ? "active" : "not-active"
+                      }
+                      onClick={toggleMenu}
+                    >
+                      <img src={user} alt="user" className="d-none" />
+                      <span>Account</span>
+                    </Link>
+                  </div>
                 </div>
-              </Link>
-            </div>
+              </div>
+            )}
           </div>
         </div>
       </header>
@@ -154,4 +348,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default header;
