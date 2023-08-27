e714f0962ae8b6955a7baba3054459becb42abbd
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-08-27 21:02:15 +0100

    update

--- src/components/choices.jsx
+++ src/components/choices.jsx
@@ -14,12 +14,13 @@ const choices = () => {
       <div className="row">
         <div className="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2 hero-details">
           <div className="row g-4 justify-content-center justify-content-md-between align-items-center">
+
             <div className='col-6 col-md-4 col-lg-2'>
               <div className='card text-center m-auto'>
                     <img src={courier} alt="" className='img-fluid m-auto' />
                     <div className="card-body">
                       <p className="card-text mb-0">Fast Orders</p>
-                  </div>
+                    </div>
               </div>
             </div>
 
@@ -32,24 +33,33 @@ const choices = () => {
             </div>
           </div>
 
-            <div className="card-details mx-3">
-                <div className='card p-2'>
-                  <img src={saving} alt="" className='img-fluid m-auto' />
-                  <p>High Saves</p>
-                </div>
-            </div>
-            <div className="card-details mx-3">
-                <div className='card p-2'>
-                  <img src={support} alt="" className='img-fluid m-auto' />
-                  <p>24/7 Support</p>
-                </div>
-            </div>
-            <div className="card-details mx-3">
-              <div className='card p-2'>
-                <img src={discount} alt="" className='img-fluid m-auto' />
-                <p>Discounts</p>
+          <div className="col-6 col-md-4 col-lg-2">
+            <div className="card text-center m-auto">
+              <img src={saving} className="card-img-top img-fluid m-auto" alt="High Saves" />
+              <div className="card-body">
+                <p className="card-text mb-0">High Saves</p>
               </div>
             </div>
+          </div>
+
+          <div className="col-6 col-md-4 col-lg-2">
+            <div className="card text-center m-auto">
+              <img src={support} className="card-img-top img-fluid m-auto" alt="24/7 Support" />
+              <div className="card-body">
+                <p className="card-text mb-0">24/7 Support</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-6 col-md-4 col-lg-2">
+            <div className="card text-center m-auto">
+              <img src={discount} className="card-img-top img-fluid m-auto" alt="Online Orders" />
+              <div className="card-body">
+                <p className="card-text mb-0">Discounts</p>
+              </div>
+            </div>
+          </div>
+
           </div>
         </div>
 
