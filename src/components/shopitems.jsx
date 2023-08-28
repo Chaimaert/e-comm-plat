1ea0e86c1235966a6aa6a93f7c27c649f00f2c89
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-08-28 13:04:33 +0100

    Update

--- src/components/shopitems.jsx
+++ src/components/shopitems.jsx
@@ -1,15 +1,19 @@
-import  Prod from './prod'
-import { PRODUCTS, PRODUCTS1 } from './products'
+/* eslint-disable react-hooks/rules-of-hooks */
+import { PRODUCTS } from './products';
+import { PRODUCTS1 } from './products';
+import Prod from './prod';
 
 
 const shopitems = () => {
+
   return <>
-  <div className="row">
-    {[...PRODUCTS, ...PRODUCTS1].map((product) => (
-      <Prod key={product.id} data={product} />
-    ))}
-  </div>
+  <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+  {[...PRODUCTS, ...PRODUCTS1].map((product) => (
+    <Prod key={product.id} data={product} />
+  ))}
+  
+        </div>
   </>
 }
 
-export default shopitems;
+export default shopitems
