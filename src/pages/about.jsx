eb8f26c5ae4a3d0db73e2230b94ae0f41be53a66
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-08-28 17:54:35 +0100

    Update

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -61,7 +61,7 @@ const about = () => {
             <div className="container-xxl">
               <div className="row">
                 <div className="col-12 d-flex flex-column align-items-center">
-                  <h1 className='mb-3'>What our customers say...</h1>
+                  <h1 className='mb-3'>What our customers say ...</h1>
                   <p className='mb-4'>Our customers never miss a bit on providing feedback</p>
                 </div>
                 <div className="col-12 col-md-6 p-3">
@@ -73,7 +73,7 @@ const about = () => {
                     <div className="col-md-8">
                       <div className="card-body">
                         <h5 className="card-title">Mark T.</h5>
-                        <p className="card-text mb-3"> <br />I&#39;m absolutely thrilled with my shopping experience on Noza Digital Marketing <br />The platform offers an incredible variety of products, and I easily found everything I needed for my home.</p>
+                        <p className="card-text mb-3"> <br />I&#39;m absolutely thrilled with my shopping experience on Noza Digital Marketing. <br />The platform offers an incredible variety of products, and I easily found everything I needed for my home.</p>
                         <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
                       </div>
                       </div>
@@ -107,7 +107,7 @@ const about = () => {
                           <div className="col-md-8">
                             <div className="card-body">
                               <h5 className="card-title">Emily Swolf</h5>
-                              <p className="card-text mb-3">Shopping at Noza Digital Marketing has been a delightful experience. <br />. The platform&#39;s intuitive layout and wide selection of products made finding what I needed effortless <br />I was impressed by the quality of the items I purchased, and the smooth checkout process.</p>
+                              <p className="card-text mb-3">Shopping at Noza Digital Marketing has been a delightful experience. <br /> The platform&#39;s intuitive layout and wide selection of products made finding what I needed effortless <br />I was impressed by the quality of the items I purchased, and the smooth checkout process.</p>
                               <p className="card-text"><small className="text-body-secondary">Last Updated 2 days ago</small></p>
                             </div>
                           </div>
@@ -124,7 +124,7 @@ const about = () => {
                           <div className="col-md-8">
                             <div className="card-body">
                               <h5 className="card-title">Phillip Omay</h5>
-                              <p className="card-text mb-3">Noza Digital Marketing has exceeded my expectations in every way possible <br />.  The diverse range of products available, <br /> combined with the platform&#39;s user-friendly design, made my shopping journey enjoyable.</p>
+                              <p className="card-text mb-3">Noza Digital Marketing has exceeded my expectations in every way possible. <br />The diverse range of products available, <br /> combined with the platform&#39;s user-friendly design, made my shopping journey enjoyable.</p>
                               <p className="card-text"><small className="text-body-secondary">Last Updated 22h ago</small></p>
                             </div>
                           </div>
@@ -141,8 +141,8 @@ const about = () => {
                           <div className="col-md-8">
                             <div className="card-body">
                               <h5 className="card-title">Melisa Ivy</h5>
-                              <p className="card-text mb-3">From electronics to fashion, I found exactly what I was looking for <br />. The exceptional customer service and prompt delivery further solidified my trust in Noza Digital Marketing <br />It&#39;s a one-stop shop that I highly recommend to everyone!.</p>
-                              <p className="card-text"><small className="text-body-secondary">Last Updated now</small></p>
+                              <p className="card-text mb-3">From electronics to fashion, I found exactly what I was looking for.<br /> The exceptional customer service and prompt delivery further solidified my trust in Noza Digital Marketing <br />It&#39;s a one-stop shop that I highly recommend to everyone!.</p>
+                              <p className="card-text"><small className="text-body-secondary">Last Updated 10 days ago</small></p>
                             </div>
                           </div>
                         </div>
@@ -157,9 +157,9 @@ const about = () => {
                         </div>
                         <div className="col-md-8">
                           <div className="card-body">
-                            <h5 className="card-title">Marvel Clein</h5>
-                            <p className="card-text mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit <br />. Doloribus maxime consequatur, ipsam architecto incidunt volup <br />tate! Iste ipsa numquam quos nam quibusdam perferendis excepturi rem, a quo laudantium libero dolore nisi.</p>
-                            <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+                            <h5 className="card-title">Alex Mark</h5>
+                            <p className="card-text mb-3">My experience with Noza Digital Marketing has been exceptional.<br />Navigating through their extensive collection of products was a breeze,I appreciated the detailed product descriptions that helped me make informed choices.</p>
+                            <p className="card-text"><small className="text-body-secondary">Last updated 45 mins ago</small></p>
                           </div>
                           </div>
                       
