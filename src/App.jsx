5fa10aa027b69d7893689cc24b34f1602dfc4cdc
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-08-30 00:36:42 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,55 @@
-import './App.css'
+import React from 'react'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 import Layout from './components/layout'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
+import About from './pages/about'
 import Shop from './pages/shop'
 import Contact from './pages/contact'
-import About from './pages/about'
-import Cart from './pages/cart'
 import Login from './pages/login'
-import Shopcontext from './components/shopcontext'
+import Signup from './pages/signup'
+import Forgotpassword from './pages/forgotpassword'
+import Cart from './pages/cart'
+import Checkout from './pages/checkout'
+import './App.css'
+import ShopContext from './components/shopcontext'
 import Details from './pages/details'
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
 
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 
 function App() {
-  return  <>
-    <Shopcontext>
-      <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Layout />}>
-              <Route index element={<Home />}  />
-              <Route path='shop' element={<Shop />} />
-              <Route path='contact' element={<Contact />} />
-              <Route path='about' element={<About />} />
-              <Route path='cart' element={<Cart />} />
-              <Route path='login' element={<Login />} />
-              <Route path='details' element={<Details />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-    </Shopcontext>
+
+  return (
+    <>
+    <ShopContext>
+    <BrowserRouter>
+    <ScrollToTop />
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='login' element={<Login />} />
+        <Route path='signup' element={<Signup />} />
+        <Route path='forgotpasword' element={<Forgotpassword />} />
+        <Route path='cart' element={<Cart />} />
+        <Route path='checkout' element={<Checkout />} />
+        <Route path='details' element={<Details />} />
+      </Route>
+    </Routes>
+    </BrowserRouter>
+    </ShopContext>
     </>
+  )
 }
 
 export default App
