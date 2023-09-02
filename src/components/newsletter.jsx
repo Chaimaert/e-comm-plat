fd085283df56f6ab12b2d6c5435eace2a692259e
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-09-02 15:44:02 +0100

    Update

--- src/components/newsletter.jsx
+++ src/components/newsletter.jsx
@@ -2,13 +2,13 @@ import { Link } from 'react-router-dom';
 
 const newsletter = () => {
   return <>
-  <section className="news-letter p-5">
+  <section className="news-letter p-5 mb-3">
     <div className="container-xxl">
       <div className="row g-3">
         <div className="col-md-6">
           <div className="d-flex flex-column align-items-center">
-            <h2 className='mb-3'>Sign Up for a newsletter</h2>
-            <h5>Get email updates on all our <Link>special offers</Link></h5>
+            <h2 className=' sign mb-3'>Sign Up for a newsletter</h2>
+            <h5>Get email updates on all our <Link className='text-danger special fs-5'>special offers</Link></h5>
           </div>
         </div>
         <div className="col-md-6 details d-flex flex-column justify-content-center">
