ea433e74a8ade5027461c3f6dc58f0958e489887
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-09-11 12:44:29 +0100

    Update

--- src/components/Cartitems.jsx
+++ src/components/Cartitems.jsx
@@ -21,7 +21,7 @@ const cartitems = (props) => {
         </div>
         <div className="col-12 col-md-7">
           <div className="p-3">
-            <h2>{name}</h2>
+            <h2 className="name mb-4">{name}</h2>
             <p className="cart-item-id">Product Brand: <b className='text-center mb-1'>{brand}</b></p>
             <p className="cart-item-id">Product Price: <b className='text-center mb-1'>${price}</b></p>
             <p className="cart-item-id">Product Number: <b className='text-center mb-3'>{id}</b></p>
