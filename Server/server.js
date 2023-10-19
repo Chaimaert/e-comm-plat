5c1212a790f3b6277f9a3b8e391c93ab5f64376f
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-10-19 23:00:09 +0100

    Update

---
+++ Server/server.js
@@ -0,0 +1,12 @@
+import  express  from "express";
+
+
+
+
+const app = express();
+
+app.get("/", (req,res) => {
+  res.send("API is Running .... ");
+})
+
+app.listen(5000,console.log("Server is Running ..."))
