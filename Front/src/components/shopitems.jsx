d52b96c4695f7eec1c5430ffffd94212d016c0dd
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-10-20 22:43:34 +0100

    Update

--- Front/src/components/shopitems.jsx
+++ Front/src/components/shopitems.jsx
@@ -1,10 +1,20 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-import { PRODUCTS } from '../data/products';
-import { PRODUCTS1 } from '../data/products';
+
+import { useEffect, useState } from 'react';
 import Prod from './prod';
+import axios from "axios";
 
 
 const shopitems = () => {
+
+  const [products, setProducts] = useState([])
+
+  useEffect(()=> {
+    const fetchproducts = async() => {
+      const {data} = axios.get("/api/products");
+      setProducts(data);
+    };
+
+  },[])
 
   return <>
   <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
