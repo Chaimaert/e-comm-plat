c02b6135c86b5cc8e55f7c990b6cf0c182ec5526
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-10-22 22:53:28 +0100

    Update Mongodb.js

--- Server/config/MongoDb.js
+++ Server/config/MongoDb.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 const connectDatabase = async () => {
   try {
     
-    const connection = await mongoose.connect(process.env.MONGO_URL)
+    const connection = await mongoose.connect(process.env.MONGO_URL, {
+      useUnifiedTopology:true,
+      useNewUrlParser:true,
+    })
 
   } catch (error) {
     console.log(`Error: ${error.message}`)
