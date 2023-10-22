69980127fd652628504ae76c18a5a157160df0a8
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-10-22 11:48:52 +0100

    Update

--- Front/src/components/shopitems.jsx
+++ Front/src/components/shopitems.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { PRODUCTS, PRODUCTS1 } from '../data/products';
+import { PRODUCTS } from '../data/products';
 import Prod from './prod';
 
 
@@ -7,7 +7,7 @@ const shopItems = () => {
 
   return <>
   <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
-  {[...PRODUCTS, ...PRODUCTS1].map((product) => (
+  {[...PRODUCTS].map((product) => (
     <Prod key={product.id} data={product} />
   ))}
   
