6041d5d888259fb0af5efb782d14ae87c5474e65
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-10-22 23:13:00 +0100

    Creating UserModel

---
+++ Server/Models/UserModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  
+  name: {
+    type:String,
+    require:true
+  },
+  email: {
+    type:String,
+    require:true,
+    unique:true
+  },
+  password: {
+    type:String,
+    require:true
+  },
+  isAdmin: {
+    type:Boolean,
+    require:true,
+    default:false
+  }
+},
+
+)
