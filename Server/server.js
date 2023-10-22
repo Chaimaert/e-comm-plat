606f01a4fc093801b61fa9dda3d5ce3c5c753396
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-10-22 22:59:40 +0100

    Update

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,11 @@
-import  express  from "express";
-import cors from 'cors'; 
+import express from "express";
+import cors from "cors";
 import products from "./data/Products.js";
 import dotenv from "dotenv";
+import connectDatabase from "./config/MongoDb.js";
 
-
-dotenv.config()
+dotenv.config();
+connectDatabase();
 
 const app = express();
 
@@ -14,18 +15,18 @@ app.use(cors());
 // To load products from the server
 app.get("/api/products", (req, res) => {
   res.json(products);
-})
+});
 
 // single product from the server
 app.get("/api/products/:id", (req, res) => {
   const product = products.find((p) => p.id === req.params.id);
   res.json(product);
-})
+});
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
   res.send("API is Running ...");
-})
+});
 
 const PORT = process.env.PORT || 1000;
 
-app.listen(PORT, console.log(`Server run in port ${PORT}`))
+app.listen(PORT, console.log(`Server run in port ${PORT}`));
