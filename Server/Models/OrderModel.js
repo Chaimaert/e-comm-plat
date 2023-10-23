2688b634a2df7f0cdd5717c29d4221d98142c661
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-10-23 23:06:30 +0100

    Update

--- Server/Models/OrderModel.js
+++ Server/Models/OrderModel.js
@@ -1,44 +1,48 @@
 import mongoose from "mongoose";
 
-const orderSchema = mongoose.Schema({
-
-  user: {
-    type:mongoose.Schema.Types.ObjectId,
-    require:true,
-    ref:"User",
-  },
-  OrderItems: [
-    {
-      name: { type: String, require:true },
-      qty:  { type: Number, require:true },
-      image: { type: String, require:true,},
-      price:  { type: Number, require:true },
-      product: {
-          type:mongoose.Schema.Types.ObjectId,
-          require:true,
-          ref:"Product",
-      },
+const orderSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      require: true,
+      ref: "User",
     },
-  ],
-  ShippingAddress: {
-      address: { type: String, require:true },
-      city: { type: String, require:true,},
-      postalCode:  { type: Number, require:true },
-      country: { type: String, require:true },
+    OrderItems: [
+      {
+        name: { type: String, require: true },
+        qty: { type: Number, require: true },
+        image: { type: String, require: true },
+        price: { type: Number, require: true },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          require: true,
+          ref: "Product",
+        },
+      },
+    ],
+    ShippingAddress: {
+      address: { type: String, require: true },
+      city: { type: String, require: true },
+      postalCode: { type: Number, require: true },
+      country: { type: String, require: true },
+    },
+    paymentMethod: {
+      type: String,
+      require: true,
+      default: "Paypal",
+    },
+    paymentResult: {
+      id: { type: String },
+      status: { type: String },
+      update_time: { type: String },
+      email_address: { type: String },
+    },
   },
-  paymentMethod: {
-    type:String,
-    require:true,
-    default:"Paypal",
-  },
 
-},
-
-{
-  timestamps:true
-}
-
-)
+  {
+    timestamps: true,
+  }
+);
 
 const Order = mongoose.model("Order", orderSchema);
 
