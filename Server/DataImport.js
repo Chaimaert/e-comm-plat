94a70fc69b00da7fd62ffdf188c6848e8d8b1c0f
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-10-23 23:42:57 +0100

    Update

--- Server/DataImport.js
+++ Server/DataImport.js
@@ -1,8 +1,11 @@
 import express from "express";
 import User from './Models/UserModel';
+import users from './data/users';
 
 const ImportData = express.Router()
 
-ImportData.post("/user", (req,res)=> {
-  await User.Remove({})
-})
+ImportData.post("/user", async (req,res)=> {
+  await User.Remove({});
+  const importUser = await User.insertMany(users);
+  res.send({ importUser });
+});
