d2c5c5c31396529b4d077dbcfa0246d5bc67a4b8
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-10-27 11:59:45 +0100

    Update

--- Server/Middleware/Error.js
+++ Server/Middleware/Error.js
@@ -1,16 +1,17 @@
 
+
 const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
-  next(error);
+  next(error); 
 };
 
-const errorHandler = (req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+const errorHandler = (error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
   res.json({
-    message: errorHandler.message,
-    stack: process.env.NODE_ENV === "production" ? null : errorHandler.stack,
+    message: error.message,
+    stack: process.env.NODE_ENV === "production" ? null : error.stack,
   });
 };
 
