2089b50c5aa6f9e8edf9178ae72119bc6c43b610
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-10-27 11:02:23 +0100

    Update

--- Server/Routes/ProductRoutes.js
+++ Server/Routes/ProductRoutes.js
@@ -1,4 +1,12 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
+import Product from "../Models/ProductModel.js"
 
-const productRoute =  express.Router()
+const productRoute =  express.Router();
+
+productRoute.get("/", asyncHandler(
+  async() => {
+    const products = await Product.find({})
+  }
+)
+)
