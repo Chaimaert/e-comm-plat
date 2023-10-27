4dfea12558b8a3149950f8b255c80d70511ae873
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-10-27 11:16:58 +0100

    Update ProductRoutes

--- Server/Routes/ProductRoutes.js
+++ Server/Routes/ProductRoutes.js
@@ -4,6 +4,8 @@ import Product from "../Models/ProductModel.js"
 
 const productRoute =  express.Router();
 
+// To get all the products
+
 productRoute.get(
   "/", 
   asyncHandler(
@@ -13,12 +15,20 @@ productRoute.get(
   })
 );
 
+// To get a single products
+
 productRoute.get(
   "/:id", 
   asyncHandler(
   async(req, res) => {
-    const products = await Product.find({});
-    res.json(products);
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    }else {
+      res.status(404);
+      throw new Error("Product not Found!");
+    }
+
   })
 );
 
