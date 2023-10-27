307083ba87d8d6797d72e692673f02d7a6fb56bc
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-10-27 12:30:30 +0100

    Update

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+import store from './Redux/store.js';
+import { Provider } from 'react-redux';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
