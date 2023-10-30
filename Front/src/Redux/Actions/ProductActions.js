14b48ea62a3bebdcc3df0d3fd61ceb077fc85d14
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-10-30 13:03:16 +0100

    Update

--- Front/src/Redux/Actions/ProductActions.js
+++ Front/src/Redux/Actions/ProductActions.js
@@ -1,11 +1,12 @@
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_FAIL } from "../constants/ProductConstants";
+import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_FAIL, PRODUCT_LIST_SUCCESS } from "../constants/ProductConstants";
 import axios from 'axios';
 
 export const listProduct = () => async (dispatch) => {
   try {
-    dispatch({type: PRODUCT_LIST_REQUEST})
+    dispatch({type: PRODUCT_LIST_REQUEST});
 
-    const {data} =  await axios.get
+    const {data} =  await axios.get("/api/products")
+    dispatch({type: PRODUCT_LIST_SUCCESS, payload: data});
   } catch (error) {
     dispatch({
       type : PRODUCT_LIST_FAIL,
