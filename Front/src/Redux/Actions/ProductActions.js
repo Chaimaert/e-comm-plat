bea1d6419450afc446511f70d04e7131069accf0
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-10-30 12:59:34 +0100

    Update

--- Front/src/Redux/Actions/ProductActions.js
+++ Front/src/Redux/Actions/ProductActions.js
@@ -1,8 +1,9 @@
+import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_FAIL } from "../constants/ProductConstants";
 
 
 export const listProduct = () => async (dispatch) => {
   try {
-    
+    dispatch({type: PRODUCT_LIST_REQUEST})
   } catch (error) {
     dispatch({
       type : PRODUCT_LIST_FAIL,
