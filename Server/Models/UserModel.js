a229dce602a0314fd34daa59f2474bebedb70c04
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-11-04 11:06:51 +0100

    Update

--- Server/Models/UserModel.js
+++ Server/Models/UserModel.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
 
 const userSchema = mongoose.Schema({
 
@@ -27,6 +29,14 @@ const userSchema = mongoose.Schema({
 }
 
 )
+
+
+//Login
+userSchema.methods.matchPassword = async function (enterPassword) {
+  return await bcrypt.compare(enterPassword, this.password);
+};
+
+
 
 const User = mongoose.model("User", userSchema);
 
