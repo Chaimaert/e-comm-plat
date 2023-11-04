cf70784e815515dc229c8db168cbdb40014c00e2
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-11-04 13:12:22 +0100

    Update

--- Server/utils/generateToken.js
+++ Server/utils/generateToken.js
@@ -1,9 +1,8 @@
 import jwt from "jsonwebtoken";
 
 const generateToken = (id) => {
-  return 
-    jwt.sign({id}, process.env.JWT_SECRET), 
+  return jwt.sign({id}, process.env.JWT_SECRET), 
     {
         expireIn: "30d",
-    }
-}
+    };
+};
