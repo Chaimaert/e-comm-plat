95bf7d9f117d841f2a30d31bbae391fb50779169
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-11-04 11:11:50 +0100

    Update

--- Server/Routes/UserRoutes.js
+++ Server/Routes/UserRoutes.js
@@ -12,8 +12,18 @@ productRoute.get(
     const { email, password } = req.body;
     const user = await User.findOne({ email });
 
-    if (condition) {
-      
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: null,
+        createAt: user.createAt
+      });
+    }else {
+      res.status(401)
+      throw new Error("Invalid Email or Password")
     }
   })
 );
