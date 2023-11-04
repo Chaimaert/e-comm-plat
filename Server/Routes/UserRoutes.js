1b66e08a0d9b77202203ead86bab7ef2fd04d12e
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-11-04 11:00:08 +0100

    Update

--- Server/Routes/UserRoutes.js
+++ Server/Routes/UserRoutes.js
@@ -4,8 +4,7 @@ import Product from "../Models/ProductModel.js"
 
 const userRoute =  express.Router();
 
-// To get all the products
-
+// Login
 productRoute.get(
   "/", 
   asyncHandler(
@@ -15,21 +14,5 @@ productRoute.get(
   })
 );
 
-// To get a single products
-
-productRoute.get(
-  "/:id", 
-  asyncHandler(
-  async(req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    }else {
-      res.status(404);
-      throw new Error("Product not Found!");
-    }
-
-  })
-);
 
 export default userRoute;
