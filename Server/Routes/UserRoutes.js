70967641d0655d606d3febb1282b704838749c22
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-11-04 13:35:22 +0100

    Update

--- Server/Routes/UserRoutes.js
+++ Server/Routes/UserRoutes.js
@@ -27,4 +27,25 @@ userRouter.post(
   })
 );
 
+userRouter.post(
+  "/login",
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+        createdAt: user.createdAt,
+      });
+    } else {
+      res.status(401).json({ message: "Invalid Email or Password" });
+    }
+  })
+);
+
 export default userRouter;
