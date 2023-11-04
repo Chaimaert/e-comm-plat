bcfdc3831e94874612798607f6e20c1069c3a431
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-11-04 13:16:15 +0100

    Update

--- Server/Routes/UserRoutes.js
+++ Server/Routes/UserRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
 import User from "../Models/UserModel.js";
+import generateToken from '../utils/generateToken.js';
 
 const userRouter = express.Router();
 
@@ -17,7 +18,7 @@ userRouter.post(
         name: user.name,
         email: user.email,
         isAdmin: user.isAdmin,
-        token: null,
+        token: generateToken,
         createAt: user.createAt,
       });
     } else {
