b06b2d50284d004441fca3b246a5d053ed98a05c
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-11-04 13:50:30 +0100

    Update

--- Server/Routes/UserRoutes.js
+++ Server/Routes/UserRoutes.js
@@ -27,24 +27,11 @@ userRouter.post(
   })
 );
 
-userRouter.post(
-  "/login",
+//PROFILE
+userRouter.get(
+  "/profile",
   asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-
-    if (user && (await user.matchPassword(password))) {
-      res.json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin,
-        token: generateToken(user._id),
-        createdAt: user.createdAt,
-      });
-    } else {
-      res.status(401).json({ message: "Invalid Email or Password" });
-    }
+    res.send("User Profile");
   })
 );
 
