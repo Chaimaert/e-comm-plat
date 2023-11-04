2695965aaa54ca66d328b3d24f3ce216eb9bd01a
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-11-04 14:12:21 +0100

    Update

--- Server/Middleware/AuthMiddleware.js
+++ Server/Middleware/AuthMiddleware.js
@@ -2,17 +2,19 @@ import jwt from "jsonwebtoken";
 import asyncHandler from "express-async-handler";
 import User from "../Models/UserModel.js";
 
-
 const protect = asyncHandler(async (req, res, next) => {
   let token;
 
   if (
-    req.headers.authorization && req.headers.authorization.startsWith("Bearer")
-    
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
   ) {
-    console.log("Token Found")
+    console.log("Token Found");
   }
-
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
 });
 
-
+export default protect;
