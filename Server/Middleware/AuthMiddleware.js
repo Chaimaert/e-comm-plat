c03ae7b1722f8a14cdf451f9ef480669d232060b
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-11-04 14:36:56 +0100

    Update

--- Server/Middleware/AuthMiddleware.js
+++ Server/Middleware/AuthMiddleware.js
@@ -13,8 +13,9 @@ const protect = asyncHandler(async (req, res, next) => {
       token = req.headers.authorization.split(" ")[1];
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      console.log(decoded);
 
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
     } catch (error) {
       console.error(error);
       res.status(401);
