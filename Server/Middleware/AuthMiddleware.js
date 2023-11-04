df6cda470f4c2713be1a90db13509dce1966e1fe
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-11-04 14:20:54 +0100

    Update

--- Server/Middleware/AuthMiddleware.js
+++ Server/Middleware/AuthMiddleware.js
@@ -9,7 +9,14 @@ const protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
-    console.log("Token Found");
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
   }
   if (!token) {
     res.status(401);
