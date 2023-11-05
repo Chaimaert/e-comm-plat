81903e9a90ddf801888a3e83c498ec7dd7120c82
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-11-05 11:24:10 +0100

    Update

--- Server/Models/UserModel.js
+++ Server/Models/UserModel.js
@@ -37,7 +37,13 @@ userSchema.methods.matchPassword = async function (enterPassword) {
 };
 
 //REGISTRATION
-
+userSchema.pre("save", async function(next){
+  if (!this.isModified("password")){
+    next()
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt); 
+})
 
 
 
