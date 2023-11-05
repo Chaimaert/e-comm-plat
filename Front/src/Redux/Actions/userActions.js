25277e4d184841e425c3f28c849630e595256225
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-11-05 13:16:25 +0100

    Update

--- Front/src/Redux/Actions/userActions.js
+++ Front/src/Redux/Actions/userActions.js
@@ -1,11 +1,10 @@
 import axios from "axios";
-import { USER_LOGIN_REQUEST } from "../constants/UserConstants";
-
-
-
+import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST,
+         USER_LOGIN_SUCCESS } 
+         from "../constants/UserConstants";
 
 //LOGIN
-export const login = (email,password) => async (dispatch) => {
+export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({ type: USER_LOGIN_REQUEST });
 
@@ -15,12 +14,16 @@ export const login = (email,password) => async (dispatch) => {
       },
     };
 
-    const { data } = await axios.post("http://localhost:5000/api/users/login",);
+    const { data } = await axios.post(
+      "http://localhost:5000/api/users/login",
+      { email, password },
+      config
+    );
 
-    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
   } catch (error) {
     dispatch({
-      type: PRODUCT_LIST_FAIL,
+      type: USER_LOGIN_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
