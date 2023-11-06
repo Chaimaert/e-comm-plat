6b82069abda7af9c35ff5609a4092f3c13c1b7f7
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-11-06 16:54:05 +0100

    Update

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -35,7 +35,6 @@ function App() {
           <Routes>
             <Route path="/" element={<Layout />}>
               <Route index element={<Home />} />
-              <Route path="/login" element={<Login />} />
               <Route path="shop" element={<Shop />} />
               <Route path="about" element={<About />} />
               <Route path="contact" element={<Contact />} />
