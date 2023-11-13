c78aa311efe1508c31d4edc4be85a74874caea0c
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-11-13 23:20:14 +0100

    Update

--- Front/src/Redux/Actions/userActions.js
+++ Front/src/Redux/Actions/userActions.js
@@ -39,3 +39,35 @@ export const login = (email, password) => async (dispatch) => {
     });
   }
 };
+
+//LOGIN
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: USER_LOGIN_REQUEST });
+
+    const config = {
+      headers: {
+        "content-Type": "application/json",
+      },
+    };
+
+    const { data } = await axios.post(
+      "http://localhost:5000/api/users/login",
+      { email, password },
+      config
+    );
+
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+
+    localStorage.setItem("userInfo", JSON.stringify(data));  
+
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
