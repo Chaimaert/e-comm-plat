09fd1aaed285c5591570dfab4cf7be0536833625
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-11-13 23:22:38 +0100

    Update

--- Front/src/Redux/Actions/userActions.js
+++ Front/src/Redux/Actions/userActions.js
@@ -40,10 +40,10 @@ export const login = (email, password) => async (dispatch) => {
   }
 };
 
-//LOGIN
-export const login = (email, password) => async (dispatch) => {
+//REGISTER
+export const register = (email, password) => async (dispatch) => {
   try {
-    dispatch({ type: USER_LOGIN_REQUEST });
+    dispatch({ type: USER_REGISTER_REQUEST });
 
     const config = {
       headers: {
@@ -57,13 +57,13 @@ export const login = (email, password) => async (dispatch) => {
       config
     );
 
-    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+    dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
 
     localStorage.setItem("userInfo", JSON.stringify(data));  
 
   } catch (error) {
     dispatch({
-      type: USER_LOGIN_FAIL,
+      type: USER_REGISTER_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
