9d719461712da5a2c6df8f3de1597af29640067c
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-11-13 23:07:28 +0100

    Update

--- Front/src/Redux/Reducers/userReducers.js
+++ Front/src/Redux/Reducers/userReducers.js
@@ -3,6 +3,9 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_SUCCESS
 } from "../constants/UserConstants";
 
 //LOGIN
@@ -24,11 +27,11 @@ export const userLoginReducer = (state = {}, action) => {
 //REGISTER
 export const userRegisterReducer = (state = {}, action) => {
   switch (action.type) {
-    case USER_LOGIN_REQUEST:
+    case USER_REGISTER_REQUEST:
       return { ...state, loading: true };
-    case USER_LOGIN_SUCCESS:
+    case USER_REGISTER_SUCCESS:
       return { loading: false, userInfo: action.payload };
-    case USER_LOGIN_FAIL:
+    case USER_REGISTER_FAIL:
       return { loading: false, error: action.payload };
     case USER_LOGOUT:
       return {};
