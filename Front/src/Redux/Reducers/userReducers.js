23f7e1d500d867a3175372456bd9ff42aebd4c9d
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-11-13 23:05:12 +0100

    Update

--- Front/src/Redux/Reducers/userReducers.js
+++ Front/src/Redux/Reducers/userReducers.js
@@ -20,3 +20,19 @@ export const userLoginReducer = (state = {}, action) => {
       return state;
   }
 };
+
+//REGISTRATION
+export const userRegistrationReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_LOGIN_REQUEST:
+      return { ...state, loading: true };
+    case USER_LOGIN_SUCCESS:
+      return { loading: false, userInfo: action.payload };
+    case USER_LOGIN_FAIL:
+      return { loading: false, error: action.payload };
+    case USER_LOGOUT:
+      return {};
+    default:
+      return state;
+  }
+};
