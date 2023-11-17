432bf8d78616f25ad337141349abb0965e7483e2
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-11-17 13:07:33 +0100

    Update

--- Front/src/Redux/Reducers/CartReducers.js
+++ Front/src/Redux/Reducers/CartReducers.js
@@ -1,12 +1,14 @@
-import { CART_SAVE_SHIPPING_ADDRESS } from "../constants/CartConstants"
+import { CART_SAVE_SHIPPING_ADDRESS } from "../constants/CartConstants";
 
-
-export const cartReducer = (state = { cartitems: [], shippingAddress : {} },
+export const cartReducer = (
+  state = { cartitems: [], shippingAddress: {} },
   action
-   ) => {
+) => {
+  switch (action.type) {
     case CART_SAVE_SHIPPING_ADDRESS:
       return {
         ...state,
         shippingAddress: action.payload,
       };
-   }
+  }
+};
