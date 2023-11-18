a4cf5c1b83c5086e54b0c8e08106c50f1e7ce49e
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-11-18 13:26:53 +0100

    Create PlaceOrder page

---
+++ Front/src/pages/PlaceOrder.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+import header from "../components/header";
+
+const PlaceOrderScreen = () => {
+  window.scrollTo(0, 0);
+
+  const placeOrderHandler = (e) => {
+    e.preventDefault();
+  }
+}
