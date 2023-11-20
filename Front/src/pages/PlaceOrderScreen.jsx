955359533174a375feed0408b0968ed46f01d260
Chaimae Rouita <129564701+Chaimaert@users.noreply.github.com>
2023-11-21 00:00:58 +0100

    update

--- Front/src/pages/PlaceOrderScreen.jsx
+++ Front/src/pages/PlaceOrderScreen.jsx
@@ -30,7 +30,7 @@ const PlaceOrderScreen = () => {
                   }
                 })}
               </div>
-            </div>;
+            </div>
           ) : (
             <div></div>
           )}
